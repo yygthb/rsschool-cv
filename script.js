07566d14ba68141eeaa869ce9574580c4b685245
Yura Ya <yura.ya.dev@gmail.com>
2021-12-29 13:55:54 +0300

    fix: singleQuotes in js-files by prettier

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const body = document.querySelector("body");
-const burgerButton = document.querySelector(".burger-button");
-const navMenu = document.querySelector(".header-nav");
-const navMenuItem = document.querySelectorAll(".nav-link");
+const body = document.querySelector('body');
+const burgerButton = document.querySelector('.burger-button');
+const navMenu = document.querySelector('.header-nav');
+const navMenuItem = document.querySelectorAll('.nav-link');
 
-burgerButton.addEventListener("click", menuShowToggle);
+burgerButton.addEventListener('click', menuShowToggle);
 
 navMenuItem.forEach((menuItem) => {
-  menuItem.addEventListener("click", () => {
-    burgerButton.classList.remove("active");
-    navMenu.classList.remove("active");
-    body.classList.remove("body-lock");
+  menuItem.addEventListener('click', () => {
+    burgerButton.classList.remove('active');
+    navMenu.classList.remove('active');
+    body.classList.remove('body-lock');
   });
 });
 
 function menuShowToggle() {
-  burgerButton.classList.toggle("active");
-  navMenu.classList.toggle("active");
-  body.classList.toggle("body-lock");
+  burgerButton.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  body.classList.toggle('body-lock');
 }
